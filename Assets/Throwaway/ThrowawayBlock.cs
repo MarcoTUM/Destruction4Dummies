ca981427ec41388f220b104838a4a9d984f1ec68
ga58len <ga58len@mytum.de>
2019-11-16 02:45:52 +0100

    Very basic wood/stone block script.

---
+++ Assets/Throwaway/ThrowawayBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowawayBlock : MonoBehaviour
+{
+    public float secondsToDestroy = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(gameObject, secondsToDestroy);
+        }
+    }
+}
