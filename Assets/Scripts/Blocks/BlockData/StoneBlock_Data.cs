1744685a467f9185834ef5694340d55bf7d07084
Jonathan <jjjooonnn@gmail.com>
2019-12-07 14:21:06 +0100

    Serializable
    Added system serializable to stone block data script

--- Assets/Scripts/Blocks/BlockData/StoneBlock_Data.cs
+++ Assets/Scripts/Blocks/BlockData/StoneBlock_Data.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class StoneBlock_Data : Block_Data
 {
     public override BlockType BlockType => BlockType.Stone;
