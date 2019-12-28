b9705f18f078fd4360c3396d5763a8394b59c234
Liou <liou.bao@tum.de>
2019-12-28 13:56:49 +0100

    Added Advisor model + some Animations

---
+++ Assets/Scripts/PlayScene/Advisor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AdvisorAnimator))]
+public class Advisor : MonoBehaviour
+{
+    private AdvisorAnimator animator;
+
+    private void Awake()
+    {
+        animator = this.GetComponent<AdvisorAnimator>();
+    }
+
+    private void Start()
+    {
+        if (Gamemaster.Instance.GetLevelType() != LevelType.Main)
+        {
+            this.gameObject.SetActive(false);
+            return;
+        }
+    }
+
+    public void Initialize()
+    {
+        if (Gamemaster.Instance.GetLevelType() != LevelType.Main)
+            return;
+        Vector2Int startPlatformCoord = Gamemaster.Instance.GetLevel().GetLevelData().StartPlatformCoordinates;
+        Debug.Log(startPlatformCoord.y);
+        this.transform.position = new Vector3(startPlatformCoord.x - Block_Data.BlockSize, startPlatformCoord.y + Block_Data.BlockSize/2f, 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.StopTalking();
+    }
+
+    public void HandlePlayerDeath()
+    {
+        animator.BeDisappointed();
+    }
+
+}
---
+++ Assets/Scripts/PlayScene/AdvisorAnimator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AdvisorAnimator : MonoBehaviour
+{
+    #region AnimatorHashes
+    //States:
+    private static readonly int idleState = Animator.StringToHash("Idle");
+    private static readonly int talkState1 = Animator.StringToHash("Talking1");
+    private static readonly int talkState2 = Animator.StringToHash("Talking2");
+    private static readonly int disappointedState = Animator.StringToHash("Disappointed");
+
+    //Params:
+    private static readonly int talkingParam = Animator.StringToHash("Talking");
+
+    #endregion
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+    
+    public void StartTalking()
+    {
+        animator.SetBool(talkingParam, true);
+        animator.Play(talkState1);
+    }
+
+    public void StopTalking()
+    {
+        animator.SetBool(talkingParam, false);
+    }
+
+    public void BeDisappointed()
+    {
+        animator.Play(disappointedState);
+    }
+}
