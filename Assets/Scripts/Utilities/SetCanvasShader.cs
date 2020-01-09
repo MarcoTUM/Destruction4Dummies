6bec722f6d0fa20b184fabe012db08f7757ecaeb
lee <liou@gmx.at>
2020-01-09 22:38:37 +0100

    DialogueBubbles always in front

---
+++ Assets/Scripts/Utilities/SetCanvasShader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCanvasShader : MonoBehaviour
+{
+    [SerializeField] private Shader shader;
+
+    private void Awake()
+    {
+        if (shader)
+            Canvas.GetDefaultCanvasMaterial().shader = shader;
+    }
+}
