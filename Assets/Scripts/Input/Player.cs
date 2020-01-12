a8328cc5a1a7fbc0cd9c280a81c309ec20dfd4b4
Jonathan <jjjooonnn@gmail.com>
2020-01-12 21:28:01 +0100

    Player charging effect from charge block

--- Assets/Scripts/Input/Player.cs
+++ Assets/Scripts/Input/Player.cs
@@ -58,6 +58,7 @@ public class Player : DialogueParticipant
 
     //charge block
     public bool canDestroy { get; private set; } = true;
+    private GameObject isChargingParticleEffect;
     #endregion
 
     #region Start, Update, onEnable
@@ -83,6 +84,7 @@ public class Player : DialogueParticipant
     private void OnDisable()
     {
         StopCoroutine("ChargeBlock");
+        Destroy(isChargingParticleEffect);
     }
 
     #endregion
@@ -412,6 +414,13 @@ public class Player : DialogueParticipant
 
     private IEnumerator ChargeBlock(float time)
     {
+        //charge effect
+        isChargingParticleEffect = Instantiate(EffectManager.Instance.GetEffect(10), model.transform, false);
+        isChargingParticleEffect.transform.Translate(0, 0.5f, 0);
+        Debug.Log("Charge effect info:");
+        Debug.Log("Position of model is: " + model.transform.position);
+        Debug.Log("Position of fx is: " + isChargingParticleEffect.transform.position);
+
         canDestroy = false;
         yield return new WaitForSeconds(time);
         canDestroy = true;
