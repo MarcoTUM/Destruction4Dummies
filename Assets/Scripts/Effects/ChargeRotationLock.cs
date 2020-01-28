0aa2e8bc173287a25175c8fc14ab1b96fb211d3b
Jonathan <jjjooonnn@gmail.com>
2020-01-28 12:58:55 +0100

    New charge effect
    Added a ring around the existing effect to indicate the radius more clearly.
    
    Ran spell check through story dialogues

---
+++ Assets/Scripts/Effects/ChargeRotationLock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeRotationLock : MonoBehaviour
+{
+    public Quaternion rotation;
+    void Update()
+    {
+        transform.rotation = rotation;
+    }
+}
